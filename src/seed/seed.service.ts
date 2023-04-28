e0ba4fed0215e87bbcfcc8df94c29b5b85cee8c7
ivan retamozo <retamozoi445@gmail.com>
2023-04-27 21:17:06 -0300

    antes de insercion en base de datos

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokemonResponse } from './interfaces/pokemon.response.interface';
+
+@Injectable()
+export class SeedService {
+  private readonly axios: AxiosInstance = axios;
+
+  public async executeSeed() {
+    const { data } = await this.axios.get<PokemonResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=15 ',
+    );
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+      console.log(name, no);
+    });
+    return data.results;
+  }
+}
